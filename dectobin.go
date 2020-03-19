760586dd5c3c3feb6027a06d86127e875f2daaf4
Shiewhun <shiewhunnues@gmail.com>
2020-03-19 11:39:12 +0100

    StartNG Go task 1

--- dectobin.go
+++ dectobin.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	fmt.Println("Enter an integer in base10 to be converted to binary: ")
-	var number int
+	var number int64
 	fmt.Scan(&number)
 	fmt.Printf("%b", number)
 }
